5e3ab58182f3d6f915f44951dcb53c3e1e8e3147
Shakkyra <diegosebastiangc10@gmail.com>
2022-09-29 13:30:07 +0000

    Primera practica

--- fork.c
+++ fork.c
@@ -1,14 +1,19 @@
 #include <unistd.h>
 #include <stdio.h>
+#include <sys/wait.h>
+
 int main(){
     int pid = fork();//Process id - 0 = hijo
 
     if (pid == 0){
         printf("Soy el proceso hijo\n");
-        sleep(60);
+        execl("hola.exe", "hola.exe", NULL);
+        sleep(5);
+        printf("nunca nunca nunca");
     } else
     {
         printf("Soy el proceso padre\n");
-        };
+        wait(NULL);
+        }
         return 0;
     };
