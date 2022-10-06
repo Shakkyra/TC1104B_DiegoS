ee4fd15932db5cfa3a3367ea6036e9f98d360914
Shakkyra <diegosebastiangc10@gmail.com>
2022-10-06 13:20:14 +0000

    Practica 2

--- signal.c
+++ signal.c
@@ -1,19 +1,20 @@
 #include <stdio.h>
 #include <signal.h>
 #include <unistd.h>
-int i = 1;
+int cond;
 void hola(int signalNumb){
     printf("Recibi la señal %d \n" , signalNumb);
-    i = 2;
+    cond = 2;
 }
 
 int main(){
+    cond = 1;
     signal(12,hola);
     
     while(cond == 1){ //while infinito
         printf("\nTrabajando\n");
         sleep(1);
     }
-    printf("Aquí nunca llega");
+    printf("\nAquí nunca llega\n");
     return 0;
 }
